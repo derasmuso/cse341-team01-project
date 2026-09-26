d24b510cb77c5246dcf0fe4f778e04945b16c48f
derasmuso <drasmuson@byupathway.edu>
2026-09-26 09:54:06 -0600

    I have reordered the routes in ejs-route.js

--- src/routes/ejs-routes.js
+++ src/routes/ejs-routes.js
@@ -8,14 +8,14 @@ const router = Router();
 // List all trips
 router.get('/', listTripsPage);
 
-// Trip details page
-router.get('/:tripId', tripDetailsPage);
-
 // Book ticket
 router.get('/booking/:scheduleId', bookingPage);
 router.post('/book', processBookingRequest);
 
 // Booking confirmation page
 router.get('/confirmation/:confirmationId', confirmationPage);
+
+// Trip details page
+router.get('/:tripId', tripDetailsPage);
 
 export default router;
